a9e8519ba6710e22636bfb46893f95d7448ec096
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-26 17:51:55 +0200

    add CTA

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -1,8 +1,8 @@
 <script>
 export default {
     name: "CallToAction",
-    data () {
-        
+    data() {
+
 
         return {}
     }
@@ -10,9 +10,22 @@ export default {
 </script>
 
 <template>
+    <!-- move call to action to footer -->
+    <div class="callToAction text-white mt-5 py-5">
+        <div class="container h-100 d-flex flex-column justify-content-center align-items-center">
+            <h5 class="text-center mt-5">CHANGE A LIFE TODAY</h5>
+            <div class="bar mb-4">
+            </div>
+            <p class="text-center">As long as poverty & inequality persist, none of us can truly rest. It doesn't take much to
+                change a life. Get in touch today and start making the difference</p>
+            <div class="buttons">
+                <button type="button" class="btn btn-outline-light rounded-0 me-4">VOLUNTEER</button>
+                <button type="button" class="btn btn-outline-light rounded-0">DONATE NOW</button>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
