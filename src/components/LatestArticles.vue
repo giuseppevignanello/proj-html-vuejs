02963269bd23ffc0308f3f668ddb7ead6d262cb7
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-27 16:27:44 +0200

    add view more articles component

--- src/components/LatestArticles.vue
+++ src/components/LatestArticles.vue
@@ -1,17 +1,17 @@
 <script>
 import ArticlesRightSide from "./ArticlesRightSide.vue"
 import ArticlesLeftSide from "./ArticlesLeftSide.vue"
+import ViewMoreArticles from "./ViewMoreArticles.vue"
 export default {
     name: "LatestArticles",
     components: {
         ArticlesRightSide,
-        ArticlesLeftSide
+        ArticlesLeftSide,
+        ViewMoreArticles
     },
     data() {
 
-
         return {
-            viewMoreClicked: false,
             latestArticles: [
                 {
                     image_path: "../../public/img/photo-1444664597500-035db93e2323-177x142.jpg",
@@ -57,10 +57,6 @@ export default {
 
             ]
         }
-    }, methods: {
-        click() {
-            this.viewMoreClicked = !this.viewMoreClicked
-        }
     }
 }
 </script>
@@ -78,20 +74,7 @@ export default {
                 <ArticlesRightSide :latest-articles="latestArticles" />
             </div>
         </div>
-        <div @click="click()" class="viewAll text-center py-2 mt-5 text-white">
-            View All Our Articles
-        </div>
-        <div v-show="this.viewMoreClicked" class="moreArticles">
-            <div class="d-flex justify-content-center mt-3">
-                <div class="article d-flex row mb-3" v-for="article in this.latestArticles.slice(5, 9)">
-                    <img class="col-8 col-sm-6 col-lg-4" :src="article.image_path" alt="">
-                    <div class="text col-8">
-                        <span class="text-warning d-none d-sm-block">{{ article.title }}</span>
-                        <p class="d-none d-lg-block">{{ article.subtitle }}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <ViewMoreArticles :latest-articles="latestArticles" />
     </div>
 </template>
 
---
+++ src/components/ViewMoreArticles.vue
@@ -0,0 +1,41 @@
+
+
+<script>
+export default {
+    name: "ViewMoreArticles",
+    props: {
+        latestArticles: Array
+    },
+    data() {
+
+
+        return {
+            viewMoreClicked: false,
+        }
+    }, 
+    methods: {
+        click() {
+            this.viewMoreClicked = !this.viewMoreClicked
+        }
+    }
+}
+</script>
+
+<template>
+    <div @click="click()" class="viewAll text-center py-2 mt-5 text-white">
+        View All Our Articles
+    </div>
+    <div v-show="this.viewMoreClicked" class="moreArticles">
+        <div class="d-flex justify-content-center mt-3">
+            <div class="article d-flex row mb-3" v-for="article in this.latestArticles.slice(5,this.latestArticles.length)">
+                <img class="col-8 col-sm-6 col-lg-4" :src="article.image_path" alt="">
+                <div class="text col-8">
+                    <span class="text-warning d-none d-sm-block">{{ article.title }}</span>
+                    <p class="d-none d-lg-block">{{ article.subtitle }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
