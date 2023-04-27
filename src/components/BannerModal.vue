c9996e79f83f3754007c006925e3c1f434c6ab71
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-27 17:38:12 +0200

    add banner Modal component

---
+++ src/components/BannerModal.vue
@@ -0,0 +1,35 @@
+
+<script>
+export default {
+    name: "BannerModal",
+    props: {
+        bannerText: Array
+    },
+    data () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<template>
+   <div class="modal fade" id="bannerTextModal" tabindex="-1" aria-labelledby="bannerTextModal" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="bannerTextModal">Every Moment Counts</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p v-for="p in this.bannerText">{{ p }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/PageBanner.vue
+++ src/components/PageBanner.vue
@@ -1,6 +1,10 @@
 <script>
+import BannerModal from './BannerModal.vue';
 export default {
     name: "PageBanner",
+    components: {
+        BannerModal
+    },
     data() {
 
         return {
@@ -96,19 +100,7 @@ export default {
             </div>
         </div>
         <!-- Modal -->
-        <div class="modal fade" id="bannerTextModal" tabindex="-1" aria-labelledby="bannerTextModal" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="bannerTextModal">Every Moment Counts</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p v-for="p in this.bannerText">{{ p }}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <BannerModal :banner-text="bannerText"/>
     </div>
 </template>
 
