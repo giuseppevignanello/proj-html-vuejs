46b8acb43ebade7bb68e55f4accc71038afec069
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-27 14:14:01 +0200

    make articles section responsive

--- src/components/ArticlesLeftSide.vue
+++ src/components/ArticlesLeftSide.vue
@@ -17,7 +17,7 @@ export default {
             <img src="../../public/img/photo-1444213007800-cff19e1677ac-400x300.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title text-warning my-3">Understanding community complexities</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et sunt libero
+                <p class="card-text ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et sunt libero
                     ullam ipsam commodi officia.</p>
             </div>
         </div>
--- src/components/ArticlesRightSide.vue
+++ src/components/ArticlesRightSide.vue
@@ -17,10 +17,10 @@ export default {
     <div class="right_side d-flex flex-column">
         <div class="article d-flex row mb-3" v-for="article in this.latestArticles
                     ">
-            <img class="col-4" :src="article.image_path" alt="">
+            <img class="col-8 col-sm-6 col-lg-4" :src="article.image_path" alt="">
             <div class="text col-8">
-                <span class="text-warning">{{ article.title }}</span>
-                <p>{{ article.subtitle }}</p>
+                <span class="text-warning d-none d-sm-block">{{ article.title }}</span>
+                <p class="d-none d-lg-block">{{ article.subtitle }}</p>
             </div>
         </div>
     </div>
