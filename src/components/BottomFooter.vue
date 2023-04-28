d061c7770774fb1424ed3b20c5c49b23efed7036
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-28 11:50:42 +0200

    show angle up only on the scroll down

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -2,13 +2,28 @@
 export default {
     name: "BottomFooter",
     props: {
-        text: Array, 
+        text: Array,
         socialIcon: Array
     },
     data() {
 
 
-        return {}
+        return {
+            scrolledDown: false
+        }
+    },
+    mounted() {
+        let InitScrollPosition = 0;
+        window.addEventListener('scroll', () => {
+            if ((document.body.getBoundingClientRect()).top < InitScrollPosition) {
+                this.scrolledDown = true;
+            }
+            else {
+                this.scrolledDown = false;
+            }
+            InitScrollPosition = (document.body.getBoundingClientRect()).top;
+        });
+
     }
 }
 </script>
@@ -18,18 +33,19 @@ export default {
         <div class="container text-center py-5">
             <div class="text">
                 <span class="icon">
-                    <font-awesome-icon icon="fa-regular fa-copyright" class="me-2"/>
+                    <font-awesome-icon icon="fa-regular fa-copyright" class="me-2" />
                 </span>
                 <span v-for="item in text">
                     {{ item }}
                 </span>
             </div>
             <div class="social d-flex justify-content-center my-4">
-                <font-awesome-icon v-for="icon in socialIcon" :icon="icon" class="icon fa-xl me-3 text-warning rounded-circle bg-light p-2" />
+                <font-awesome-icon v-for="icon in socialIcon" :icon="icon"
+                    class="icon fa-xl me-3 text-warning rounded-circle bg-light p-2" />
             </div>
         </div>
-        <div class="angleUp rounded-circle d-flex align-items-center">
-            <a href="#" class="px-2 "> <font-awesome-icon  class="w-100 h-100" :icon="['fas', 'angle-up']" /></a>
+        <div v-if="this.scrolledDown == true" class="angleUp rounded-circle d-flex align-items-center">
+            <a href="#" class="px-2 "> <font-awesome-icon class="w-100 h-100" :icon="['fas', 'angle-up']" /></a>
         </div>
     </div>
 </template>
