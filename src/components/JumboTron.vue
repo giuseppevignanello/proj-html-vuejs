30d0db2bc41dc9088665ed5a4d643036c0cdfc9c
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-28 11:53:38 +0200

    remove offCanvas Jumbotron

--- src/components/JumboTron.vue
+++ src/components/JumboTron.vue
@@ -23,16 +23,11 @@ export default {
             <p class="text-center"> {{text}}</p>
             <div class="buttons row justify-content-center px-2">
                 <button type="button" class="col-12 col-md-4 btn btn-outline-light mx-2 rounded-0">{{first_btn}}</button>
-                <button type="button" class="col-12 col-md-4 btn btn-outline-warning mx-2 rounded-0" data-bs-toggle="offcanvas"
-                    data-bs-target="#donate" aria-controls="donate">{{second_btn}}</button>
+                <button type="button" class="col-12 col-md-4 btn btn-outline-warning mx-2 rounded-0">
+                  <a class="text-white" href="#Donate">{{second_btn}}</a>  
+                </button>
             </div>
         </div>
-
-
-
-         <!-- Offcanvas -->
-        
-        <OffcanvasDonate/>
 
     </div>
 </template>
