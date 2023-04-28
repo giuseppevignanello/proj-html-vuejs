99b119a31f03ed52e57a6e015a30fda9497abea5
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-28 15:05:19 +0200

    add latestArticles in a data folder

--- src/components/LatestArticles.vue
+++ src/components/LatestArticles.vue
@@ -3,6 +3,7 @@ import ArticlesRightSide from "./ArticlesRightSide.vue"
 import ArticlesLeftSide from "./ArticlesLeftSide.vue"
 import ViewMoreArticles from "./ViewMoreArticles.vue"
 import SectionTitle from "./SectionTitle.vue"
+import latestArticles from "../data/latestArticles"
 export default {
     name: "LatestArticles",
     components: {
@@ -15,50 +16,8 @@ export default {
 
         return {
             title: "Latest Articles",
-            latestArticles: [
-                {
-                    image_path: "../../public/img/photo-1444664597500-035db93e2323-177x142.jpg",
-                    title: "The Human story of uniqueness",
-                    subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
-                },
-                {
-                    image_path: "../../public/img/photo-1447430617419-95715602278e-177x142.jpg",
-                    title: "Sustainable trade tactics ",
-                    subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
-                },
-                {
-                    image_path: "../../public/img/photo-1460230525622-630fe3294cd7-177x142.jpg",
-                    title: "Farmers making a difference ",
-                    subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
-                },
-                {
-                    image_path: "../../public/img/photo-1460600421604-5e138c208b9c-177x142.jpg",
-                    title: "Meeting remote tribes in Perù",
-                    subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
-                },
-                {
-                    image_path: "../../public/img/photo-1444664597500-035db93e2323-177x142.jpg",
-                    title: "The Human story of uniqueness",
-                    subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
-                },
-                {
-                    image_path: "../../public/img/photo-1447430617419-95715602278e-177x142.jpg",
-                    title: "Sustainable trade tactics ",
-                    subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
-                },
-                {
-                    image_path: "../../public/img/photo-1460230525622-630fe3294cd7-177x142.jpg",
-                    title: "Farmers making a difference ",
-                    subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
-                },
-                {
-                    image_path: "../../public/img/photo-1460600421604-5e138c208b9c-177x142.jpg",
-                    title: "Meeting remote tribes in Perù",
-                    subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
-                },
-
-
-            ]
+            latestArticles: latestArticles,
+            
         }
     }
 }
---
+++ src/data/latestArticles.js
@@ -0,0 +1,45 @@
+export default 
+ [
+    {
+        image_path: "../../public/img/photo-1444664597500-035db93e2323-177x142.jpg",
+        title: "The Human story of uniqueness",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
+    },
+    {
+        image_path: "../../public/img/photo-1447430617419-95715602278e-177x142.jpg",
+        title: "Sustainable trade tactics ",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
+    },
+    {
+        image_path: "../../public/img/photo-1460230525622-630fe3294cd7-177x142.jpg",
+        title: "Farmers making a difference ",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
+    },
+    {
+        image_path: "../../public/img/photo-1460600421604-5e138c208b9c-177x142.jpg",
+        title: "Meeting remote tribes in Perù",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
+    },
+    {
+        image_path: "../../public/img/photo-1444664597500-035db93e2323-177x142.jpg",
+        title: "The Human story of uniqueness",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
+    },
+    {
+        image_path: "../../public/img/photo-1447430617419-95715602278e-177x142.jpg",
+        title: "Sustainable trade tactics ",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
+    },
+    {
+        image_path: "../../public/img/photo-1460230525622-630fe3294cd7-177x142.jpg",
+        title: "Farmers making a difference ",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
+    },
+    {
+        image_path: "../../public/img/photo-1460600421604-5e138c208b9c-177x142.jpg",
+        title: "Meeting remote tribes in Perù",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure modi unde, optio similique autem."
+    },
+
+
+]
